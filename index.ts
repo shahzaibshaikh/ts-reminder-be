9722d6758483017517a0479cd40748458f0337d9
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-20 00:58:48 +0500

    👌 IMPROVE: added express router file

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-
+import reminderRouter from './routers/reminders';
 const app = express();
+
+app.use('/reminders', reminderRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello world.');
+});
 
 const PORT = 8000;
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
